6026c8020ac8e93229a52d7000c7f8c8ad7ab935
Nisal-Sashmitha <IT20081034@my.sliit.lk>
2022-04-15 19:40:41 +0530

    added basic apis for crud in interruption

--- Interruption Calendar And Notification Service/src/main/java/com/InterruptionCalenderService.java
+++ Interruption Calendar And Notification Service/src/main/java/com/InterruptionCalenderService.java
@@ -6,12 +6,15 @@ import model.Interruptions;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 
 @Path("/Interruptions")
 public class InterruptionCalenderService {
 	
 	
-	Interruptions itemObj = new Interruptions();
+	Interruptions interruptionObj = new Interruptions();
 	@GET
 	@Path("/")
 	@Produces(MediaType.TEXT_HTML)
@@ -19,6 +22,56 @@ public class InterruptionCalenderService {
 	 {
 		return "Hello";
 	 }
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String insertItem(String itemData)
+	{
+		//Convert the input string to a JSON object
+		 JsonObject itemObject = new JsonParser().parse(itemData).getAsJsonObject();
+		//Read the values from the JSON object
+		
+		 String date = itemObject.get("date").getAsString();
+		 String startTime = itemObject.get("startTime").getAsString();
+		 String endTime = itemObject.get("endTime").getAsString();
+		 String areaID = itemObject.get("areaID").getAsString();
+		 String output = interruptionObj.newInterruption(date, startTime, endTime, areaID);
+		 return output;
+	}
+	
+	@PUT
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updateInterruption(String interruptionData)
+	{
+		//Convert the input string to a JSON object
+		JsonObject interruptionDataObject = new JsonParser().parse(interruptionData).getAsJsonObject();
+    	//Read the values from the JSON object
+		String InterruptionID = interruptionDataObject.get("InterruptionID").getAsString();
+		String date = interruptionDataObject.get("date").getAsString();
+		String startTime = interruptionDataObject.get("startTime").getAsString();
+		String endTime = interruptionDataObject.get("endTime").getAsString();
+		String areaID = interruptionDataObject.get("areaID").getAsString();
+		String output = interruptionObj.editInterruptions(InterruptionID, date, startTime, endTime, areaID);
+		return output;
+	}
+	
+	@DELETE
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String deleteItem(String interruptionData)
+	{
+		//Convert the input string to a JSON object
+		JsonObject interruptionDataObject = new JsonParser().parse(interruptionData).getAsJsonObject();
+		//Read the value from the element <itemID>
+		String InterruptionID = interruptionDataObject.get("interruptionID").getAsString();
+		String output = interruptionObj.deleteInterruption(InterruptionID);
+		return output;
+	}
+
 	
 
 }
--- Interruption Calendar And Notification Service/src/main/java/model/Interruptions.java
+++ Interruption Calendar And Notification Service/src/main/java/model/Interruptions.java
@@ -10,10 +10,9 @@ public class Interruptions {
 	private String areaID;
 	
 	
-	public String newInterruption(int interruptionID, String date, String startTime, String endTime, String areaID) {
+	public String newInterruption( String date, String startTime, String endTime, String areaID) {
 		String output = "";
-		String query = "insert into interruption('interruptionID', 'date', 'startTime', 'endTime', 'areaID' )"
-		+"values(?,?,?,?,?) ";
+		String query = "INSERT INTO `electronicgriddb`.`interruption` (`date`, `startTIme`, `endTime`, `areaID`) VALUES (?, ?, ?, ?); ";
 		
 		
 		try {
@@ -22,13 +21,14 @@ public class Interruptions {
 			
 			PreparedStatement preparedStmt = con.prepareStatement(query);
 			// binding values
-			preparedStmt.setInt(1, 0);
-			preparedStmt.setString(2, date);
-			preparedStmt.setString(3, startTime);
-			preparedStmt.setString(4, endTime);
-			preparedStmt.setString(5, areaID);
+			
+			preparedStmt.setString(1, date);
+			preparedStmt.setString(2, startTime);
+			preparedStmt.setString(3, endTime);
+			preparedStmt.setString(4, areaID);
 			
 			preparedStmt.execute();
+			
 			con.close();
 			output = "Inserted successfully";
 			
@@ -40,5 +40,74 @@ public class Interruptions {
 		
 		return output;
 	}
+	
+	public void interruptionsOfTheConsumer(String NIC) {}
+	
+	public void viewAllInterruptionsOfADay() {}
+	
+	public String editInterruptions(String interruptionID, String date, String startTime, String endTime,String areaID) {
+		String output = "";
+		 try
+		 {
+			 SQLDatabase db = new SQLDatabase();
+			 Connection con = db.getConnection();
+			 if (con == null)
+			 {return "Error while connecting to the database for updating."; }
+			 
+			 // create a prepared statement
+			 String query = "UPDATE `electronicgriddb`.`interruption` SET `date` = ?, `startTIme` = ?, `endTime` = ?, `areaID` = ? WHERE (`interruptionID` = ?);";
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 // binding values
+			 preparedStmt.setString(1, date);
+			 preparedStmt.setString(2, startTime);
+			 preparedStmt.setString(3, endTime);
+			 preparedStmt.setString(4, areaID);
+			 preparedStmt.setString(5, interruptionID);
+        	// execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 output = "Updated successfully";
+		 }
+		 catch (Exception e)
+		 {
+			 output = "Error while updating the item.";
+			 System.err.println(e.getMessage());
+		 }
+		 return output; 
+
+	}
+	
+	public String deleteInterruption(String interruptionID) {
+		
+		String output = "";
+		try
+		 {
+			 SQLDatabase db = new SQLDatabase();
+			 Connection con = db.getConnection();
+			 if (con == null)
+			 {return "Error while connecting to the database for deleting."; }
+			 // create a prepared statement
+			 String query = "delete from `interruption` where `interruptionID`=?";
+			 
+			
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+			 // binding values
+			 preparedStmt.setString(1, interruptionID);
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 output = "Deleted successfully";
+		 }
+		 catch (Exception e)
+		 {
+			 output = "Error while deleting the item.";
+			 System.err.println(e.getMessage());
+		 }
+		 return output; 
+		
+	}
+	
+	public void viewNotifications() {}
+	
 	
 }
